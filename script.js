6f6569c73748308f9cf0f17a4b49d76080ec3b21
Henrich Tanis <henrich@Henrichs-MacBook-Pro.local>
2023-02-21 19:03:10 -0600

    added the class to the times

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-var dateEl = $('#date-display');
-var timeEL = $('#time-display');
-var button = $(`.buttonz`)
+var dateEl = $("#date-display");
+var timeEL = $("#time-display");
+var button = $(`.buttonz`);
+
 // function readProjectsFromStorage() {
 //   var events = localStorage.getItem('events');
 //   if (events) {
@@ -11,39 +12,39 @@ var button = $(`.buttonz`)
 //   return events;
 // }
 
-
-
-
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  button.on("click", function () {
+    console.log("you clicked me");
+    console.log(this);
+    var clock = $(this).parent().attr("id");
+    var text = $(this).siblings("#textArea").val();
+    localStorage.setItem(clock, text);
 
-  button.on('click', function () {
-  console.log('you clicked me')
-  console.log(this);
-  var clock = $(this).parent().attr('id')
-  var text = $(this).siblings('#textArea').val();
-  localStorage.setItem(clock, text)
+    var present = $(this).parent("row");
+    var future = parseInt(present.attr("id"));
+    var current = parseInt(dayjs().format("H"));
 
-  
-  
-  // function readTxt() {
-  // var text = localStorage.getItem('events');
-  // if (events) {
-  //   text= JSON.parse(events);
-  // } else {
-  //   events = [];
-  // }
-  // return events;
-// }
+    $(this).addClass(
+      future < current ? "past" : future === current ? ".present" : "future"
+    );
+
+    // for (let i = 9; i <= 17; i++) {
+    //   $(`#hour-${i} #textArea`).val(localStorage.getItem(`hour-${i}`));
+    // }
+
+    // function readTxt() {
+    // var text = localStorage.getItem('events');
+    // if (events) {
+    //   text= JSON.parse(events);
+    // } else {
+    //   events = [];
+    // }
+    // return events;
+    // }
   });
-
-
-
-
-
-
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -64,26 +65,22 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 
-  $('#hour-9 #textArea').val(localStorage.getItem('hour-9'));
-  $('#hour-10 #textArea').val(localStorage.getItem('hour-10'));
-  $('#hour-11 #textArea').val(localStorage.getItem('hour-11'));
-  $('#hour-12 #textArea').val(localStorage.getItem('hour-12'));
-  $('#hour-13 #textArea').val(localStorage.getItem('hour-13'));
-  $('#hour-14 #textArea').val(localStorage.getItem('hour-14'));
-  $('#hour-15 #textArea').val(localStorage.getItem('hour-15'));
-  $('#hour-16 #textArea').val(localStorage.getItem('hour-16'));
-  $('#hour-17 #textArea').val(localStorage.getItem('hour-17'));
-
-  
+  $("#hour-9 #textArea").val(localStorage.getItem("hour-9"));
+  $("#hour-10 #textArea").val(localStorage.getItem("hour-10"));
+  $("#hour-11 #textArea").val(localStorage.getItem("hour-11"));
+  $("#hour-12 #textArea").val(localStorage.getItem("hour-12"));
+  $("#hour-13 #textArea").val(localStorage.getItem("hour-13"));
+  $("#hour-14 #textArea").val(localStorage.getItem("hour-14"));
+  $("#hour-15 #textArea").val(localStorage.getItem("hour-15"));
+  $("#hour-16 #textArea").val(localStorage.getItem("hour-16"));
+  $("#hour-17 #textArea").val(localStorage.getItem("hour-17"));
 });
 
 function displayTime() {
   var date = dayjs().format(`[Date:]	dddd, MMMM D, YYYY`);
-  var time = dayjs().format(` [Time:]  h:mm A`)
+  var time = dayjs().format(` [Time:]  h:mm A`);
   dateEl.text(date);
-  timeEL.text(time)
+  timeEL.text(time);
 }
 
-
-
-displayTime()
+displayTime();
